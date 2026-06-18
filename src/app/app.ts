034920f0fc5d4ac59143d9b55398188c8ad2c522
Dima Rysanow <thecyberd3m0n@gmail.com>
2026-06-18 14:04:28 +0200

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('angular-advanced-pwa-example');
+
+  constructor() {
+    setTimeout(() => this.title.set('title changed'), 3000);
+  }
+}

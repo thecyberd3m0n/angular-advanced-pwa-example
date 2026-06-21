c4e7897254b561af87e107328f31c19b8ceedd9c
klonek <piotr.kasperski94@gmail.com>
2026-06-21 15:41:07 +0200

    update on request

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,21 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NotificationService } from './notification.service';
+import { Update } from './update';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('angular-advanced-pwa-example');
   private readonly notificationService = inject(NotificationService);
+  private readonly updateService = inject(Update);
   readonly permissionStatus = this.notificationService.permissionStatus;
+  readonly isNewVersion$ = this.updateService.isNewVersion$();
 
   constructor() {
     setTimeout(() => this.title.set('title changed'), 3000);
@@ -19,5 +23,8 @@ export class App {
 
   async onRequestPermission(): Promise<void> {
     await this.notificationService.requestPermission();
+  }
+  onUpdate() {
+    this.updateService.activeUpdate();
   }
 }
